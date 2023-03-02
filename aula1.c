b87f5771ac44fd2cb1b107f08e57df37b93172da
brunopdrocha <brunopdrocha@gmail.com>
2023-03-02 16:03:54 -0300

    aula

---
+++ aula1.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+/* Print para o Hello, World
+int main (void){
+    printf("Hello, World\n");
+    return 0;
+}
+*/
+
+int main (void){
+    int x, y;  /* Variaveis*/
+    x=2;
+    y= x *3 +4;
+    printf("O resultado é %i e %i",x,y);
+    return 0;
+}
